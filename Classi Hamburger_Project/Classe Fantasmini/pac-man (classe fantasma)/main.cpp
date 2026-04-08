28a5266c9f852f67af841a9dc594506c58b8ed15
sironi09 <sironimarco2009@gmail.com>
2026-04-08 18:28:02 +0200

    Add files via upload

---
+++ Classi Hamburger_Project/Classe Fantasmini/pac-man (classe fantasma)/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+#include "fantasma.h"
+
+using namespace std;
+
+int main()
+{
+	fantasma red, orange, pink, blue;
+	cout << red.toString() << endl;
+	cout << orange.toString() << endl;
+	cout << pink.toString() << endl;
+	cout << blue.toString() << endl;
+
+}
